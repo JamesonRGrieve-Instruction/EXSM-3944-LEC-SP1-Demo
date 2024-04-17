99b03d1e42d3b276add31fac99650d27d4280b5d
James Grieve <jamesonrgrieve@gmail.com>
2024-04-16 20:48:06 -0600

    Switch to http from https;

--- src/DemoMVC.Tests/Test_Tests.cs
+++ src/DemoMVC.Tests/Test_Tests.cs
@@ -15,7 +15,7 @@ public class Test_Tests : PageTest
     [Test]
     public async Task TestGoogle()
     {       
-        await Page.GotoAsync("https://localhost:7121");
+        await Page.GotoAsync("http://localhost:5203");
         await Expect(Page).ToHaveTitleAsync("Home Page - DemoMVC");
     }
 
