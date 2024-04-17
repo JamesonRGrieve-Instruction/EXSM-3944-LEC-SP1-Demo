feb22c0f81b3698ac79254edc71f23820359f4d7
James Grieve <jamesonrgrieve@gmail.com>
2024-04-16 20:41:23 -0600

    Add playwright test for MVC;

--- src/DemoMVC.Tests/Test_Tests.cs
+++ src/DemoMVC.Tests/Test_Tests.cs
@@ -1,8 +1,6 @@
 using NUnit.Framework;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using DemoMVC.Controllers;
-using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 namespace DemoMVC.Tests;
 
@@ -12,14 +10,13 @@ public class Test_Tests : PageTest
     [SetUp]
     public void Setup()
     {
-        Controller HomeController = new HomeController();
     }
 
     [Test]
     public async Task TestGoogle()
     {       
-        await Page.GotoAsync("https://google.ca");
-        await Expect(Page).ToHaveTitleAsync("Google");
+        await Page.GotoAsync("https://localhost:7121");
+        await Expect(Page).ToHaveTitleAsync("Home Page - DemoMVC");
     }
 
 }
