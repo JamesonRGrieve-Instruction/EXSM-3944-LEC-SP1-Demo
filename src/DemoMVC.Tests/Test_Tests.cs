4320560e7b0b418bab559fd5d47aa879ac1164aa
James Grieve <jamesonrgrieve@gmail.com>
2024-04-19 20:54:44 -0600

    Test chrome and firefox;

--- src/DemoMVC.Tests/Test_Tests.cs
+++ src/DemoMVC.Tests/Test_Tests.cs
@@ -14,9 +14,12 @@ public class Test_Tests : PageTest
 
     [Test]
     public async Task TestGoogle()
-    {       
-        await Page.GotoAsync("http://localhost:5203");
-        await Expect(Page).ToHaveTitleAsync("Home Page - DemoMVC");
+    {
+        await Page.GotoAsync("https://localhost:7121/home/sample");
+        await Expect(Page).ToHaveTitleAsync("Razor Sample - DemoMVC");
+        await Task.Delay(3000);
+        await Page.FillAsync("input[name=FirstName]", "John");
+        await Task.Delay(2000);
     }
 
 }
