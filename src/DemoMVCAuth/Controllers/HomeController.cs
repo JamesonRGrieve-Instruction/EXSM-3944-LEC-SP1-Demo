08605361692298e09eaa5c16e2fcb0bb635e7315
James Grieve <jamesonrgrieve@gmail.com>
2024-04-24 10:06:10 -0600

    Setup with MySQL; Basic storing of model;

--- src/DemoMVCAuth/Controllers/HomeController.cs
+++ src/DemoMVCAuth/Controllers/HomeController.cs
@@ -6,6 +6,13 @@ namespace DemoMVCAuth.Controllers;
 
 public class HomeController : Controller
 {
+    private List<Student> students = new List<Student>() {
+        new Student() {
+            UserID= "jgrieve@ualberta.ca",
+            FirstName = "John",
+            LastName = "Doe"
+        }
+    };
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -13,9 +20,26 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+
+    public IActionResult Index([FromForm] Student student)
     {
-        return View();
+        if (Request.Method == "POST")
+        {
+            if (string.IsNullOrEmpty(student.FirstName))
+            {
+                ModelState.AddModelError("FirstName", "First Name is required.");
+            }
+            if (string.IsNullOrEmpty(student.LastName))
+            {
+                ModelState.AddModelError("LastName", "Last Name is required.");
+            }
+            if (ModelState.IsValid)
+            {
+                student.UserID = User.Identity.Name;
+                students.Add(student);
+            }
+        }
+        return View(students.Where(student => student.UserID == User.Identity.Name).ToList());
     }
 
     public IActionResult Privacy()
---
+++ src/DemoMVCAuth/Models/Student.cs
@@ -0,0 +1,6 @@
+public class Student
+{
+    public string? UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
