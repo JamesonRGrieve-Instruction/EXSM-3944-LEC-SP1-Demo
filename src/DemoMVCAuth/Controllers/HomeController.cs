2da989367f5d1b12701b36bcbe28776f474cb0fa
James Grieve <jamesonrgrieve@gmail.com>
2024-04-24 10:21:15 -0600

    Redirection if not logged in;

--- src/DemoMVCAuth/Controllers/HomeController.cs
+++ src/DemoMVCAuth/Controllers/HomeController.cs
@@ -44,6 +44,10 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return RedirectToAction(nameof(Index));
+        }
         return View();
     }
 
