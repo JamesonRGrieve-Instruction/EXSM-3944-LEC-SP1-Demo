0588406a20fd4609410fa11a89ae27e205f6da30
James Grieve <jamesonrgrieve@gmail.com>
2024-05-07 19:24:44 -0600

    Migration for PhoneNumber;

---
+++ src/DemoMVCAuth/Data/Migrations/20240508012422_PersonPhoneNumber.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoMVCAuth.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersonPhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "phone_number",
+                table: "person",
+                type: "char(12)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "person",
+                keyColumn: "id",
+                keyValue: -2,
+                column: "phone_number",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "person",
+                keyColumn: "id",
+                keyValue: -1,
+                column: "phone_number",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "phone_number",
+                table: "person");
+        }
+    }
+}
